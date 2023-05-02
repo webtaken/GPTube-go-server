722620aa00200b8ceb3380b4fd02d262bc8644ef
saul <luckly083@gmail.com>
2023-05-01 20:29:40 -0500

    removing maxBadCommentsAllowed var in Youtube Analyzer

--- server/YoutubeAnalyzer/main.go
+++ server/YoutubeAnalyzer/main.go
@@ -16,8 +16,6 @@ var huggingFaceAuthHeader = fmt.Sprintf("Bearer %s",
 	envManager.GoDotEnvVariable("HUGGING_FACE_TOKEN"))
 var mu sync.Mutex
 
-const maxBadCommentsAllowed = 10
-
 func init() {
 	ctx := context.Background()
 	service, err := youtube.NewService(ctx, option.WithAPIKey(envManager.GoDotEnvVariable("YOUTUBE_API_KEY")))
